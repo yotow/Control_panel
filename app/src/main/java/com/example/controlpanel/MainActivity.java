cdb73a0b20deafe71637efc1b63fd489790d1e7a
yotow <lxdr@ro.ru>
2020-04-23 12:53:27 +0300

    del fragments

--- app/src/main/java/com/example/controlpanel/MainActivity.java
+++ app/src/main/java/com/example/controlpanel/MainActivity.java
@@ -1,55 +1,43 @@
 package com.example.controlpanel;
 
+import android.app.Activity;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+//    WiFiActivity wiFiActivity = new WiFiActivity();
+    public void onClick(View view) {
+        TextView tv = findViewById(R.id.textView1Main);
+        if(view == findViewById(R.id.button_get_ip)){
+  //          String s = wiFiActivity.getDeviceCurrentIPAddress();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            tv.setText("");
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
+
+/*
+class WiFiActivity extends MainActivity{
+
+    public String getDeviceCurrentIPAddress() {
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        assert wifiManager != null;
+        int ip = wifiManager.getConnectionInfo().getIpAddress();
+
+        String ipString = String.format("%d.%d.%d.%d", (ip & 0xff),
+                (ip >> 8 & 0xff), (ip >> 16 & 0xff), (ip >> 24 & 0xff));
+        return ipString;
+    }
+
+}*/
+
