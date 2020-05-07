6c833255edc9f451aaa4e6b673ba46831e220366
yotow <lxdr@ro.ru>
2020-05-07 14:09:03 +0300

    Внедрение VOLLEY

--- app/src/main/java/com/example/controlpanel/MainActivity.java
+++ app/src/main/java/com/example/controlpanel/MainActivity.java
@@ -1,43 +1,87 @@
 package com.example.controlpanel;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    /*  TextView mPingTextView;
+      TextView mTextView;*/
+    EditText mEditText;
+    String mIp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mEditText = findViewById(R.id.edit_text);
     }
 
-//    WiFiActivity wiFiActivity = new WiFiActivity();
     public void onClick(View view) {
-        TextView tv = findViewById(R.id.textView1Main);
-        if(view == findViewById(R.id.button_get_ip)){
-  //          String s = wiFiActivity.getDeviceCurrentIPAddress();
+        String url = "http://" + mIp + "/socket1On";
+        doRequest(url);
 
-            tv.setText("");
-        }
     }
-}
 
+    public void onClickSetIP(View view) {
+        mIp = mEditText.getText().toString().trim();
+
+    }
+
+    public void onClickLightOff(View view) {
+        String url = "http://" + mIp + "/socket1Off";
+        doRequest(url);
+    }
+
+    void doRequest(String url) {
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
 /*
-class WiFiActivity extends MainActivity{
-
-    public String getDeviceCurrentIPAddress() {
-        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-        assert wifiManager != null;
-        int ip = wifiManager.getConnectionInfo().getIpAddress();
-
-        String ipString = String.format("%d.%d.%d.%d", (ip & 0xff),
-                (ip >> 8 & 0xff), (ip >> 16 & 0xff), (ip >> 24 & 0xff));
-        return ipString;
-    }
-
-}*/
-
+    public void doPing(View view) {
+        String ip = "192.168.5.1";
+        mPingTextView.setText("execute ping");
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process mIpAddrProcess = runtime.exec("/system/bin/ping -c 1 " + ip);
+            int mExitValue = mIpAddrProcess.waitFor();
+            mPingTextView.append("\n mExitValue " + mExitValue);
+            if (mExitValue == 0) {
+                mPingTextView.append("\nok");
+            } else {
+                mPingTextView.append("\nwrong");
+            }
+        } catch (InterruptedException ignore) {
+            ignore.printStackTrace();
+            System.out.println(" Exception:" + ignore);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(" Exception:" + e);
+        }
+    }*/
+}
